23c0af62ac80ea5a460335d3d8c8573369491085
Jay Doane <jay.s.doane@gmail.com>
2016-03-10 22:11:43 -0800

    Remove `cast cluster add` hack

--- files/cluster.sh
+++ files/cluster.sh
@@ -4,7 +4,5 @@ DB_PREFIX=${DB_PREFIX-db}
 
 for host in `grep ${DB_PREFIX}[0-9]*[0-9a-z.] /etc/hosts | awk '{ print $2 }' | uniq`
 do
-    #FIXME: cast cluster add $host
-    # hack until above command works:
-    curl -X PUT http://localhost:5986/nodes/cloudant@$host -H "Content-Type: application/json" -d '{}'
+    cast cluster add $host
 done
